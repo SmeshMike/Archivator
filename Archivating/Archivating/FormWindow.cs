3f3f84fd81aedaaac7532150b47e59f4c07b1b7f
Unknown <bowlcaketeam@yandex.ru>
2019-05-14 18:46:19 +0300

    Zdelal

---
+++ Archivating/Archivating/FormWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Archivating
+{
+    public partial class FormWindow : Form
+    {
+        public FormWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void FormWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Archivating/Archivating/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivating
+{
+    class Program
+    {
+
+        public static void StringProb(string s)
+        {
+            int length = s.Length;
+            char[] chr = s.ToCharArray();
+            int[] chr_num = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                chr_num[i] = 1;
+                for (int j = i + 1; j < length - 1; j++)
+                {
+                    while (chr[i] == chr[j])               
+                    {
+                        for (int k = j; k < length - j; k++)
+                        {
+                            chr[k - 1] = chr[k];
+                        }
+                        Array.Clear(chr, length - 1, 1);
+                        length--;
+                    }
+                }
+                
+            }
+            string s1 = new string(chr);
+            string s2 = string.Join("", chr_num);
+            Console.WriteLine(s1, "  ", s2);
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            string Str = File.ReadAllText(@"C:\SomeTXT.txt");
+            double Str_L = Str.Length;
+            StringProb(Str);
+            Console.ReadLine();
+        }
+    }
+
+   
+
+}
+
